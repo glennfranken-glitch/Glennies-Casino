05ab2d9870019ac3ac391ccc42306e92fa759e3a
Glenn Franken <glenn.franken@gmail.com>
2026-09-20 09:42:11 +0200

    Changed variables to English, added Costs, Gender and Birthday

--- Main.py
+++ Main.py
@@ -1,8 +1,36 @@
-print("Casino de Gouden Driehoek")
+firstname = input("Voornaam: ")
+lastname = input("Achternaam: ")
 
-voornaam = input("Voornaam: ")
-achternaam = input("Achternaam: ")
+firstname = firstname.capitalize()
+lastname = lastname.capitalize()
 
-voornaam = voornaam.capitalize()
-achternaam = achternaam.capitalize()
+birthday = input("Geboortedatum: dd-mm-yyyy")
 
+gender = input("Geslacht: (m/v)")
+if gender == "m":
+    salutation = "meneer", lastname
+
+elif gender == "v":
+    salutation = "mevrouw", lastname
+
+else:
+    salutation = firstname, lastname
+
+startbudget = float(input("Wat is je startbudget in euro's?"))
+
+#Vaste kosten
+ENTRY = float(12)
+FLIPFLOPS = float(5)
+SUNGLASSES = float(7)
+
+
+
+print("""Casino de Gouden Driehoek
+-------------------------
+Welkom, meneer Jansen
+
+Startbudget:    € 50.00
+Vaste kosten:   € 16.50
+Saldo:          € 33.50
+
+Je hebt nog genoeg budget voor toegang tot het casino.""")
